f989c4b0e2edafea07ecbf9c44c4446e69461797
Soumajit Choudhuri <soumajitchoudhuri@gmail.com>
2020-12-06 03:08:04 +0530

    adding initial code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `LearnCode Online React Project`,
+    description: `A React project that brushes the core concepts of web development`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -12,7 +12,6 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-      
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
