a613a6197bc1c190fb118979bc02373c3f48fb13
Soumajit Choudhuri <soumajitchoudhuri@gmail.com>
2020-12-16 19:39:14 +0530

    dummy contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from "../components/Reusable/HeroSection"
+import Coursecart from "../components/Cart/Coursecart"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="< Coding time />"
+      subtitle="Leading the new Digital World"
+      heroclass="hero-background"
+    />
+    <Coursecart courses={data.courses} />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "heromain.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    courses: allContentfulCourses {
+      edges {
+        node {
+          id
+          title
+          price
+          category
+          description {
+            description
+          }
+          image {
+            fixed(width: 200, height: 120) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
